f53a6ee64734e833ca1f19934c9aaf936f13e470
Samantha Stanworth <samanthastanworth@mail.weber.edu>
2025-01-29 10:48:33 -0700

    Added link to shape calc

---
+++ scripts/javascript.js
@@ -0,0 +1,16 @@
+function default_theme() {
+    document.getElementById("current_theme").innerHTML="Current Site Theme: Default";
+    document.getElementById("page_body").style="background-color: darkblue";
+            }
+
+function noir_theme() {
+    document.getElementById("current_theme").innerHTML="Current Site Theme: Noir";
+    document.getElementById("page_body").style="background-color:#000000";
+            }
+
+
+ function light_theme() {
+    document.getElementById("current_theme").innerHTML="Current Site Theme: Light";
+    document.getElementById("page_body").style="background-color:#4274BD";
+   // document.getElementsByClassName("header_style").color= "HEX #525252"; 
+             }
